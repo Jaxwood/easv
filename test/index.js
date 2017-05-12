4c030df3217c9f49e1285f182b9fcff3055032c4
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2017-05-12 07:22:57 +0200

    fix broken test

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 describe('test truthness', function() {
     it('true should be true', function(){
-        expect(true).toBe(false);
+        expect(true).toBe(true);
     })
 })
